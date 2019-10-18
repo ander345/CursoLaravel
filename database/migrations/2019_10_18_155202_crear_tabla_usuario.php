cd9ab5d4bfc72ebc0a3ab20b367c2b609669921f
Anderson <stone.colombia@idu.gov.co>
2019-10-18 12:56:22 -0500

    se creo migracion

---
+++ database/migrations/2019_10_18_155202_crear_tabla_usuario.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();;
+            $table->string('usuario',50);
+            $table->string('password',10);
+            $table->string('nombre',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
