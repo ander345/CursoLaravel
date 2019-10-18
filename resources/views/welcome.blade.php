2f48da6b6dfb798c1bf008ff0744c6fabb8680d2
Anderson <stone.colombia@idu.gov.co>
2019-10-18 08:11:27 -0500

    primer commit del dia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Cursos Laravel
+                    Curso Laravel
                 </div>
 
                 <div class="links">
