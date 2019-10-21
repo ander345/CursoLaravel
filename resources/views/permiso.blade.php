385b8416cd1682e594f14de546edd98ff042faf9
Anderson <stone.colombia@idu.gov.co>
2019-10-21 16:29:32 -0500

    clase 9 rutas

---
+++ resources/views/permiso.blade.php
@@ -0,0 +1,4 @@
+
+<a href="{{route('permiso')}}"> ir a permisos </a>
+<br>
+<a href="{{url('admin/sistema/permiso')}}"> ir a permisos </a>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+//Route::get('permiso/{nombre}/{slug?}', 'PermisoController@index');
+//Route::view('permiso', 'permiso');
+
+//use App\Models\Permiso;
+
+//Route::get('admin/sistema/permiso', 'PermisoController@index')->name('permiso');
+
+use App\Http\Controllers\PermisoController;
+
+Route::get('permiso/{nombre}', function ($nombre) {
+    return $nombre;
+   
+})->where('nombre', '[A-Za-z]+')->name('permiso');
